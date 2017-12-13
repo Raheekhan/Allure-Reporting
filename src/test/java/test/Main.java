4d824c43b65d04d7d77ad6da1a905f7761940e13
Raheekhan <ibrahimkhan0894@gmail.com>
2017-12-12 20:32:28 -0500

    Updated README.md [#1]

--- src/test/java/test/Main.java
+++ src/test/java/test/Main.java
@@ -1,13 +1,16 @@
 package test;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
 public class Main {
 
+    @Epic("Epic")
+    @Feature("Feature")
     @Description("This is a description")
     @Story("This is a story")
+    @Issue("Issue-412")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void testing() {
         System.out.println("Testing Allure with TestNG");
